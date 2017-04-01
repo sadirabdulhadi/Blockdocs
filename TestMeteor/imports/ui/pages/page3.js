76d5ff4eb5608fda8904584dff3dc7ba17168725
Sadir <sadeer11@gmail.com>
2017-04-01 22:57:07 +0300

    page3

---
+++ TestMeteor/imports/ui/pages/page3.js
@@ -0,0 +1,77 @@
+import './page3.html'
+import './page1.js'
+import '../../api/common.js'
+import { documentsdb } from '../../../lib/database.js';
+import { Meteor } from 'meteor/meteor';
+import { toSign } from '../../../lib/database.js';
+
+
+var ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+ETHEREUM_CLIENT.eth.defaultAccount = ETHEREUM_CLIENT.eth.accounts[1];
+var PCabi = [{"constant":false,"inputs":[{"name":"nonce","type":"string"}],"name":"generateId","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"documents","outputs":[{"name":"organizer","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"SimpleSign","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"docId","type":"bytes32"},{"name":"signId","type":"uint8"}],"name":"getSignDetails","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"docId","type":"bytes32"}],"name":"addSignature","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"docId","type":"bytes32"}],"name":"getDocumentDetails","outputs":[{"name":"organizer","type":"address"},{"name":"count","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"docId","type":"bytes32"}],"name":"getDocumentOrganizer","outputs":[{"name":"organizer","type":"address"},{"name":"count","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"docId","type":"bytes32"},{"name":"index","type":"uint256"}],"name":"getDocumentSignature","outputs":[{"name":"value","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"docId","type":"bytes32"}],"name":"removeDocument","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"dochash","type":"bytes32"}],"name":"createDocument","outputs":[{"name":"docId","type":"bytes32"}],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"docId","type":"bytes32"}],"name":"getSignsCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"payable":false,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"id","type":"bytes32"}],"name":"Created","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"docId","type":"bytes32"}],"name":"Signed","type":"event"}];
+var PCaddress = '0x7aeddbf80b9f45e541cbf5085b86f4ab7e20246b';
+var dochash = undefined;
+var userFirstName;
+var hash;
+
+Template.page3.events({
+  //upload document on click
+  'change .upload': function(event, template) {
+
+    var files = event.target.files;
+    console.log(files[0]);
+    event.preventDefault();
+    calculateHash(files);
+    var depdep = ETHEREUM_CLIENT.eth.contract(PCabi).at(PCaddress);
+    var x = depdep.getSignsCount.call("0x9bf0f031d4df49ca5723ee24de36b20a1e8f98b8683b40b4f9ba6949640007ef").c[0];
+    console.log(x);
+    for (var i =0;i<x;i++){
+      console.log("hi");
+      var z = depdep.getSignDetails.call("0x9bf0f031d4df49ca5723ee24de36b20a1e8f98b8683b40b4f9ba6949640007ef",i);
+      console.log(z);
+      var l = ETHEREUM_CLIENT.eth.accounts.indexOf(z);
+      console.log(Meteor.users.find({"profile.accounts":l}).fetch()[0].profile.firstName);
+    }
+    // all the l (one at every iteration) = the name of the person that signed
+
+  },
+
+  //sign up button
+  'click #register':function(event,template){
+     Accounts.createUser({
+      email: email,
+      password: password,
+      profile: { homeAddress: homeAddress }
+    });
+ }
+});
+
+function getBase64(files) {
+        //Read File
+        var selectedFile = files
+        //Check File is not Empty
+        if (selectedFile.length > 0) {
+            // Select the very first file from list
+            var fileToLoad = selectedFile[0];
+            // FileReader function for read the file.
+            var fileReader = new FileReader();
+            var base64;
+            var toreturn;
+            fileReader.onload = function(fileLoadedEvent) {
+                base64 = fileLoadedEvent.target.result;
+                //console.log("base 64 is");
+                //console.log(base64);
+                isCalculated = true;
+                toreturn = ETHEREUM_CLIENT.sha3(base64);
+                //console.log("to return is " + toreturn);
+                hash = toreturn;
+                console.log("hash has been calculated, it's now" + hash)
+            };
+            // Convert data to base64
+            fileReader.readAsDataURL(fileToLoad);
+        }
+    }
+
+function calculateHash(files){
+  getBase64(files);
+}
